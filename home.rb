91c88892349cf7d8c703b6f590817df22c6831be
Rovin Varshney <rovin100@gmail.com>
2013-12-24 11:39:38 +0530

    create home.rb to create Class instances

---
+++ home.rb
@@ -0,0 +1,4 @@
+require './song.rb'
+
+aSong = Song.new('Blue Eyes', 'Honey singh', 5.2)
+puts aSong.inspect
--- song.rb
+++ song.rb
@@ -5,6 +5,3 @@ class Song
     @duration = duration
   end
 end
-
-aSong = Song.new('Blue Eyes', 'Honey singh', 5.2)
-puts aSong.inspect
